f17cdf68cf86a3430eba5f32949cbc3dbb982e85
Artur Luniaka <artur01049718@gmail.com>
2025-04-14 18:50:33 +0100

    added container/added content/styled for mob

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import s from "./page.module.css";
+
 const page = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
+    <div className={s.background}>
+      <div className={s.container}>
+        <h1 className={s.title}>
+          I came from another reality where code is a weapon.
+        </h1>
+        <p className={s.text}>
+          &ldquo; I craft <span className={s.text_accent}>UI</span> like portals
+          into a game world, where every detail is part of the experience.
+          &rdquo;
+        </p>
+        <button className={s.btn}>Enter the world</button>
+      </div>
     </div>
   );
 };
