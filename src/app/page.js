1b3714db84856c88f7ceec3d8488b40bb678731d
Artur Luniaka <artur01049718@gmail.com>
2025-04-14 19:32:33 +0100

    animated subtitle

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import s from "./page.module.css";
 import { Typewriter } from "react-simple-typewriter";
+import { motion } from "framer-motion";
 
 const page = () => {
   return (
@@ -17,11 +18,16 @@ const page = () => {
             delaySpeed={100}
           />
         </h1>
-        <p className={s.text}>
+        <motion.p
+          className={s.text}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 4.5, duration: 1 }}
+        >
           &ldquo; I craft <span className={s.text_accent}>UI</span> like portals
           into a game world, where every detail is part of the experience.
           &rdquo;
-        </p>
+        </motion.p>
         <button className={s.btn}>Enter the world</button>
       </div>
     </div>
